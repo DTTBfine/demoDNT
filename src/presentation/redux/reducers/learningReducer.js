fecd242cd885a3a959064a58b7f38bdbbcd0da8b
kiennt99 <nkien2048@gmail.com>
2024-11-16 14:35:06 +0700

    refactor code

--- src/presentation/redux/reducers/learningReducer.js
+++ src/presentation/redux/reducers/learningReducer.js
@@ -2,20 +2,6 @@ import actionTypes from '../actions/actionTypes'
 
 const initState = {
     myClasses: [],
-    classInfo: {
-        id: 0,
-        class_id: '',
-        class_name: '',
-        schedule: '',
-        lecturer_id: 0,
-        max_student_amount: 0,
-        attached_code: '',
-        class_type: '',
-        start_date: '',
-        status: '' //ACTIVE
-    },
-    classInfoErr: '',
-    registerClassSuccess: false
 }
 
 const learningReducer = (state = initState, action) => {
@@ -25,26 +11,26 @@ const learningReducer = (state = initState, action) => {
                 ...state,
                 myClasses: action.data || []
             }
-        case actionTypes.REGISTER_CLASS_SUCCCESS:
-            return {
-                ...state,
-                registerClassSuccess: true
-            }
-        case actionTypes.REGISTER_CLASS_FAILED:
-            return {
-                ...state,
-                registerClassSuccess: false
-            }
-        case actionTypes.GET_BASIC_CLASS_INFO_SUCCESS:
-            return {
-                ...state,
-                classInfo: action.data
-            }
-        case actionTypes.GET_BASIC_CLASS_INFO_FAILED:
-            return {
-                ...state,
-                classInfoErr: action.data
-            }
+        // case actionTypes.REGISTER_CLASS_SUCCCESS:
+        //     return {
+        //         ...state,
+        //         registerClassSuccess: true
+        //     }
+        // case actionTypes.REGISTER_CLASS_FAILED:
+        //     return {
+        //         ...state,
+        //         registerClassSuccess: false
+        //     }
+        // case actionTypes.GET_BASIC_CLASS_INFO_SUCCESS:
+        //     return {
+        //         ...state,
+        //         classInfo: action.data
+        //     }
+        // case actionTypes.GET_BASIC_CLASS_INFO_FAILED:
+        //     return {
+        //         ...state,
+        //         classInfoErr: action.data
+        //     }
         case actionTypes.LOGOUT:
             return {
                 ...state,
