3ec80ba005eb5db00e6d5f6b792a4327d38a2240
kiennt99 <nkien2048@gmail.com>
2024-11-16 14:35:15 +0700

    refactor code

--- src/presentation/redux/reducers/learningReducer.js
+++ src/presentation/redux/reducers/learningReducer.js
@@ -11,26 +11,6 @@ const learningReducer = (state = initState, action) => {
                 ...state,
                 myClasses: action.data || []
             }
-        // case actionTypes.REGISTER_CLASS_SUCCCESS:
-        //     return {
-        //         ...state,
-        //         registerClassSuccess: true
-        //     }
-        // case actionTypes.REGISTER_CLASS_FAILED:
-        //     return {
-        //         ...state,
-        //         registerClassSuccess: false
-        //     }
-        // case actionTypes.GET_BASIC_CLASS_INFO_SUCCESS:
-        //     return {
-        //         ...state,
-        //         classInfo: action.data
-        //     }
-        // case actionTypes.GET_BASIC_CLASS_INFO_FAILED:
-        //     return {
-        //         ...state,
-        //         classInfoErr: action.data
-        //     }
         case actionTypes.LOGOUT:
             return {
                 ...state,
