903948488f81e74adc53675345e9ed7bb051a793
kiennt99 <nkien2048@gmail.com>
2024-12-08 19:15:23 +0700

    add search account

--- src/presentation/pages/SearchAccount.jsx
+++ src/presentation/pages/SearchAccount.jsx
@@ -4,6 +4,10 @@ import { useNavigation } from '@react-navigation/native'
 import { getDisplayedAvatar } from '../../utils/format'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import IconI from 'react-native-vector-icons/Ionicons'
+import * as apis from '../../data/api'
+import Spinner from 'react-native-loading-spinner-overlay'
+import { useSelector } from 'react-redux'
+import { responseCodes } from '../../utils/constants'
 
 const testData = [
     {
@@ -29,12 +33,62 @@ const testData = [
 
 const SearchAccount = () => {
     const navigate = useNavigation()
+    const [isLoading, setIsLoading] = useState(false)
+    const { token } = useSelector(state => state.auth)
+    const [searchedUsers, setSearchedUsers] = useState([])
+    const [currentPageInfo, setCurrentPageInfo] = useState(null)
 
     const [nameSearch, setNameSearch] = useState('')
 
-    const handleSearch = () => {
-        console.log('Xử lý đi')
-        setNameSearch('')
+    const loadMoreData = async () => {
+        if (currentPageInfo?.next_page && nameSearch) {
+            setIsLoading(true)
+            const response = await apis.apiSearchAccount({
+                search: nameSearch,
+                pageable_request: {
+                    page: currentPageInfo.next_page,
+                    page_size: currentPageInfo.page_size
+                }
+            })
+            setIsLoading(false)
+            console.log("response loading more data: " + JSON.stringify(response?.data?.data?.page_content))
+            if (response?.data?.meta?.code !== responseCodes.statusOK) {
+                setCurrentPageInfo(null)
+                console.log("error fetching next page: " + response?.data?.meta?.message)
+            }
+            const listUsers = response?.data?.data?.page_content
+            if (listUsers) {
+                setSearchedUsers((prevUsers) => [...prevUsers, ...listUsers]);
+                setCurrentPageInfo(response?.data?.data?.page_info)
+            }
+        }
+        
+    }
+
+    const handleSearch = async () => {
+        // setNameSearch('')
+        setIsLoading(true)
+        const response = await apis.apiSearchAccount(({
+            search: nameSearch,
+            pageable_request: {
+                page: "0",
+                page_size: "10"
+            }
+        }))
+        setIsLoading(false)
+
+        if (response?.data?.meta?.code !== responseCodes.statusOK) {
+            setSearchedUsers([])
+            setCurrentPageInfo(null)
+            return
+        }
+        const listUsers = response?.data?.data?.page_content
+        // console.log("list user response: " + JSON.stringify(listUsers))
+
+        if (listUsers) {
+            setSearchedUsers(listUsers)
+            setCurrentPageInfo(response?.data?.data?.page_info)
+        }
     }
 
     const renderAccount = ({ item, index }) => {
@@ -63,6 +117,13 @@ const SearchAccount = () => {
 
     return (
         <View>
+            <Spinner
+                visible={isLoading}
+                textContent={'Chờ xử lý...'}
+                textStyle={{
+                    color: '#FFF'
+                }}
+            />
             <View style={{
                 backgroundColor: 'white',
                 flexDirection: 'row',
@@ -92,13 +153,19 @@ const SearchAccount = () => {
                 </TouchableOpacity>
             </View>
             <View>
-                <FlatList
-                    data={testData}
+                {searchedUsers?.length > 0 ? (
+                    <FlatList
+                    data={searchedUsers}
                     keyExtractor={(item) => item.account_id}
                     renderItem={renderAccount}
                     contentContainerStyle={{ paddingBottom: 10 }}
                     showsVerticalScrollIndicator={false}
-                />
+                    onEndReached={async () => {await loadMoreData()}}
+                    onEndReachedThreshold={0.9}
+                    />
+                ) : (
+                    <Text>No results found</Text> // Optionally show a fallback message
+                )}
             </View>
         </View>
     )
