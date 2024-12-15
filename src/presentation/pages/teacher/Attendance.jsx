2dce7f42549f544735dbf4c103ccb9b93121f277
kiennt99 <nkien2048@gmail.com>
2024-12-15 23:20:45 +0700

    add set attendance

--- src/presentation/pages/teacher/Attendance.jsx
+++ src/presentation/pages/teacher/Attendance.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, StyleSheet, TouchableOpacity, Alert } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, TouchableOpacity, Alert, RefreshControl } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import * as actions from '../../redux/actions'
 import { useDispatch, useSelector } from 'react-redux'
@@ -12,12 +12,15 @@ import { responseCodes } from '../../../utils/constants'
 import * as constants from '../../../utils/constants'
 import { Picker } from '@react-native-picker/picker'
 
+const attendanceStatus = constants.attendanceStatus
+
 const Attendance = ({ route }) => {
     const { id, name, type, tabName } = route.params
     const { token, role, userId } = useSelector(state => state.auth)
     const [loadData, setLoadData] = useState(true)
     const [isLoading, setIsLoading] = useState(false)
     const [dateAttended, setDateAttended] = useState(null)
+    const [refreshing, setRefreshing] = useState(false)
     const dispatch = useDispatch()
 
     const { currentClass, attendanceDates } = useSelector(state => state.learning) //xóa đi
@@ -38,6 +41,7 @@ const Attendance = ({ route }) => {
                 token: token,
                 class_id: id
             }))
+            fetchAttendanceList(currentDate)
             setTimeout(() => {
                 setIsLoading(false)
             }, 500)
@@ -45,30 +49,44 @@ const Attendance = ({ route }) => {
         }
     }, [])
 
+
     const new_date = new Date()
     const [currentDate, setCurrentDate] = useState(formatSQLDate(new_date))
 
-    // const [attendStatus, setAttendStatus] = useState(Array.isArray(currentClass.student_accounts) && currentClass.student_accounts.length > 0 ?
-    //     Object.fromEntries(currentClass.student_accounts?.map(item => [item.student_id, true])) : {}
-    // )
-
-    const [attendStatus, setAttendStatus] = useState({})
-
-    // useEffect(() => {
-    //     if (currentClass) {
-    //         console.log("setting attend status")
-    //         setAttendStatus(Array.isArray(currentClass.student_accounts) && currentClass.student_accounts.length > 0 ?
-    //             Object.fromEntries(currentClass.student_accounts?.map(item => [item.student_id, true])) : {}
-    //         )
-    //     }
-      
-    // }, [currentClass])
-    console.log("attend status: " + JSON.stringify(attendStatus))
+    const [attendanceList, setAttendanceList] = useState({})
+    console.log("attendance list: " + JSON.stringify(attendanceList))
     const handleChangeStatus = (id) => {
-        if (dateAttended) {
-            return Alert.alert("Warning", "Không thể thay đổi trạng thái điểm danh")
+        setAttendanceList(prev => ({
+            ...prev,
+            [id]: {
+                ...prev[id], // Preserve the existing properties of the object
+                status: prev[id]?.status === attendanceStatus.present 
+                    ? attendanceStatus.unexcusedAbsence 
+                    : attendanceStatus.present // Update only the status
+            }
+        }));
+    }
+
+    const handleSetStatus = (id) => {
+        let status = ''
+        switch (attendanceList[id]?.status) {
+            case attendanceStatus.present:
+                status = attendanceStatus.excusedAbsence
+                break
+            case attendanceStatus.excusedAbsence:
+                status = attendanceStatus.unexcusedAbsence
+                break
+            case attendanceStatus.unexcusedAbsence:
+                status = attendanceStatus.present
+                break
         }
-        setAttendStatus(prev => ({ ...prev, [id]: !prev[id] }))
+        setAttendanceList(prev => ({
+            ...prev,
+            [id]: {
+                ...prev[id], // Preserve the rest of the properties
+                status: status // Update only the status
+            }
+        }));
     }
 
     const getAttendanceList = async (date) => {
@@ -85,32 +103,43 @@ const Attendance = ({ route }) => {
         return []
     }
 
-    const fetchAttendStatus = async (date) => {
-        setAttendStatus({});  // Initialize with an empty object
+    const fetchAttendanceList = async (date) => {
+        setAttendanceList({});  // Initialize with an empty object
         let currentDateAttendStatus = {};
         setIsLoading(true)
-        console.log("getting attendance list")
-        const attendanceList = await getAttendanceList(date);
+        console.log("getting attendance list: " )
+        const list = await getAttendanceList(date);
         setIsLoading(false)
-        // console.log('attendance list: ' + JSON.stringify(attendanceList))
-        if (attendanceList?.length > 0) {
-            attendanceList.forEach(item => {
-                // Use student_id as key and status as value
-                currentDateAttendStatus[item.student_id] = item.status !== constants.attendanceStatus.present;
+        if (list?.length > 0) {
+            // Create a lookup table (map) for attendanceList
+            let attendanceMap = {};
+            list.forEach(record => {
+                attendanceMap[record.student_id] = record
             });
-        } 
+
+            // Map student_accounts using the lookup table
+            currentClass?.student_accounts?.forEach(item => {
+                currentDateAttendStatus[item.student_id] = attendanceMap[item.student_id]; // Default to null if no record found
+            });
+        } else {
+            currentClass?.student_accounts?.forEach(item => {
+                currentDateAttendStatus[item.student_id] = {
+                    status: attendanceStatus.present
+                }
+            })
+        }
         
         // Update state with the object
-        setAttendStatus(currentDateAttendStatus);
+        setAttendanceList(currentDateAttendStatus);
     }
 
     useEffect(() => {
         if (currentClass?.student_accounts) {
             if (dateAttended) {
-                fetchAttendStatus(dateAttended)
+                fetchAttendanceList(dateAttended)
             } else {
                 console.log("default date here")
-                fetchAttendStatus(currentDate)
+                fetchAttendanceList(currentDate)
             }
         }
         
@@ -129,7 +158,8 @@ const Attendance = ({ route }) => {
             token: token,
             class_id: id,
             date: currentDate,
-            attendance_list: currentClass.student_accounts.map(item => attendStatus[item.student_id] ? item.student_id : null).filter(id => id !== null)
+            attendance_list: currentClass.student_accounts.map(item => 
+                attendanceList[item.student_id]?.status !== attendanceStatus.present ? item.student_id : null).filter(id => id !== null)
         }
         setIsLoading(true)
         const response = await apis.apiTakeAttendance(payload)
@@ -138,17 +168,66 @@ const Attendance = ({ route }) => {
         if (response?.data?.meta?.code === responseCodes.statusOK) {
             Alert.alert("Success", "Điểm danh thành công")
         } else {
+            console.log("error taking attendance: " + JSON.stringify(response.data))
             Alert.alert("Error", "Điểm danh không thành công")
         }
 
         console.log("payload: " + JSON.stringify(payload))
     }
 
+    const handleSetAttendanceStatus = async () => {
+        setIsLoading(true)
+        for (const id in attendanceList) {
+            if (attendanceList[id]) {
+                const response = await apis.apiSetAttendanceStatus({
+                    token: token,
+                    status: attendanceList[id]?.status,
+                    attendance_id: attendanceList[id]?.attendance_id
+                })
+                if (response?.data?.meta?.code !== responseCodes.statusOK) {
+                    console.log("error setting attendance status: " + JSON.stringify(response.data))
+                    Alert.alert("Error", "Không thể thay đổi trạng thái điểm danh")
+                    break
+                }
+            }
+        }
+        setIsLoading(false)
+        Alert.alert("Success", "Thay đổi trạng thái điểm danh thành công")
+    }
+
+    const handleRefresh = () => {
+        setRefreshing(true)
+        setTimeout(() => {
+            dispatch(actions.getClassInfo({
+                token: token,
+                role: role,
+                account_id: userId,
+                class_id: id
+            }))
+            dispatch(actions.getAttendanceDates({
+                token: token,
+                class_id: id
+            }))
+            setDateAttended(null)
+            fetchAttendanceList(currentDate)
+            setRefreshing(false)       
+        }, 500);
+
+    }
+
     return (
-        <View style={{ padding: 10, gap: 15 }}>
+        <ScrollView 
+            style={{ padding: 10, gap: 15 }}
+            refreshControl={
+                <RefreshControl
+                    refreshing={refreshing}
+                    onRefresh={handleRefresh}   
+                />
+            }
+        >
             <Spinner
                 visible={isLoading}
-                textContent={'Đang tải dữ liệu...'}
+                textContent=''
                 textStyle={{
                     color: '#FFF'
                 }}
@@ -195,16 +274,46 @@ const Attendance = ({ route }) => {
                         <View style={{ height: 400 }}>
                             <ScrollView>
                                 {
-                                    currentClass?.student_accounts?.length > 0 && currentClass?.student_accounts.map((item, index) => {
+                                    currentClass?.student_accounts?.length > 0 && 
+                                    currentClass?.student_accounts?.filter(item => attendanceList[item.student_id]?.status)?.map((item, index) => {
                                         return (
                                             <View key={index} style={{ flexDirection: 'row' }}>
                                                 <Cell isHeader={false} width={100} data={index + 1} />
                                                 <Cell isHeader={false} width={100} data={item.student_id} />
                                                 <Cell isHeader={false} width={250} data={item.first_name + ' ' + item.last_name} />
                                                 {/* presented in attend status means absence  */}
-                                            
-                                                <Cell isHeader={false} width={100} data={!attendStatus[item.student_id] ? <Icon name='check-circle' color='deepskyblue' size={24} onPress={() => handleChangeStatus(item.student_id)} /> :
-                                                    <Icon5 name='ban' color='#BB0000' size={22} onPress={() => handleChangeStatus(item.student_id)} />} />
+                                                {!dateAttended && <Cell isHeader={false} width={100} data={attendanceList[item.student_id]?.status === attendanceStatus.present ? <Icon name='check-circle' color='deepskyblue' size={24} onPress={() => handleChangeStatus(item.student_id)} /> :
+                                                    <Icon5 name='ban' color='#BB0000' size={22} onPress={() => handleChangeStatus(item.student_id)} />} />}
+                                                {dateAttended && (
+                                                    <Cell 
+                                                        isHeader={false} 
+                                                        width={100}
+                                                        data={
+                                                            attendanceList[item.student_id]?.status === attendanceStatus.present ? (
+                                                                <Icon
+                                                                    name="check-circle"
+                                                                    color="deepskyblue"
+                                                                    size={24}
+                                                                    onPress={() => handleSetStatus(item.student_id)}
+                                                                />
+                                                            ) : attendanceList[item.student_id]?.status === attendanceStatus.excusedAbsence ? (
+                                                                <Icon5
+                                                                    name="user-check"
+                                                                    color="green"
+                                                                    size={22}
+                                                                    onPress={() => handleSetStatus(item.student_id)}
+                                                                />
+                                                            ) : (
+                                                                <Icon5
+                                                                    name="ban"
+                                                                    color="#BB0000"
+                                                                    size={22}
+                                                                    onPress={() => handleSetStatus(item.student_id)}
+                                                                />
+                                                            )
+                                                        }
+                                                    />
+                                                )}
                                                 <Cell isHeader={false} width={100} data={''} />
                                                 <Cell isHeader={false} width={100} data={''} />
                                             </View>
@@ -217,17 +326,27 @@ const Attendance = ({ route }) => {
                 </ScrollView>
 
             </View>
-            {!dateAttended && (
+            {!dateAttended ? (
                 <View style={{ alignItems: 'center' }}>
                 <TouchableOpacity
                     style={[styles.button, { width: 150, backgroundColor: '#BB0000' }]}
-                    onPress={handleSubmit}>
+                    onPress={async () => {
+                        await handleSubmit()
+                    }}>
                     <Text style={{ color: "white", fontSize: 17, fontStyle: 'italic', fontWeight: 'bold', alignSelf: 'center', }}>Submit</Text>
                 </TouchableOpacity>
             </View>
-            )}
+            ) : <View style={{ alignItems: 'center' }}>
+                    <TouchableOpacity
+                        style={[styles.button, { width: 150, backgroundColor: '#BB0000' }]}
+                        onPress={async () => {
+                            await handleSetAttendanceStatus()
+                        }}>
+                        <Text style={{ color: "white", fontSize: 17, fontStyle: 'italic', fontWeight: 'bold', alignSelf: 'center', }}>Set Status</Text>
+                    </TouchableOpacity>
+                </View>}
             
-        </View>
+        </ScrollView>
     )
 }
 
